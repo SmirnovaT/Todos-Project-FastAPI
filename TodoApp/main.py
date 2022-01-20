b00f4386442903392de9e266596cfd6406fcf5a2
SmirnovaT <75753895+SmirnovaT@users.noreply.github.com>
2022-01-20 17:54:55 +0300

    ID + User ID
    ID + User ID

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -5,6 +5,7 @@ import models
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 from pydantic import BaseModel, Field
+from auth import get_current_user, get_user_exception
 
 
 app = FastAPI()
@@ -32,10 +33,25 @@ async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
 
 
+@app.get("/todos/user")
+async def read_all_by_user(user: dict = Depends(get_current_user),
+                           db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    return db.query(models.Todos)\
+        .filter(models.Todos.owner_id == user.get("id"))\
+        .all()
+
+
 @app.get('/todo/{todo_id}')
-async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+async def read_todo(todo_id: int,
+                    user: dict = Depends(get_current_user),
+                    db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
     todo_model = db.query(models.Todos)\
         .filter(models.Todos.id == todo_id)\
+        .filter(models.Todos.owner_id == user.get("id"))\
         .first()
     if todo_model is not None:
         return todo_model
@@ -43,17 +59,23 @@ async def read_todo(todo_id: int, db: Session = Depends(get_db)):
 
 
 @app.post('/')
-async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+async def create_todo(todo: Todo,
+                      user: dict = Depends(get_current_user),
+                      db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
     todo_model = models.Todos()
     todo_model.title = todo.title
     todo_model.description = todo.description
     todo_model.priority = todo.priory
     todo_model.complete = todo.complete
+    todo_model.owner_id = user.get('id')
 
     db.add(todo_model)
     db.commit()
 
     return successful_response(201)
+
 
 
 @app.put('/{todo_id')
